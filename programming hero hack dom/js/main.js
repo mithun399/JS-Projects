5d9106353381aa93b4d6b8e3404bb752882bc7be
mithun399 <msovik399@gmail.com>
2022-06-28 23:35:51 +0600

    complete

--- programming hero hack dom/js/main.js
+++ programming hero hack dom/js/main.js
@@ -1,32 +1,90 @@
-const milestoneData = JSON.parse(data).data;
+const milestonesData = JSON.parse(data).data;
 
-function loadMilestone() {
+
+function loadMilestones() {
     const milestones = document.querySelector(".milestones");
-    milestones.innerHTML = `${milestoneData.map(function(milestone){
-    return ` <div class="milestones">
-    <div class="milestone border-b">
-        <div class="flex">
-            <div class="checkbox"><input type="checkbox" /></div>
-            <div onclick="openMilestone(this)">
-                <p>
-                    ${milestone.name}
-                    <span><i class="fas fa-chevron-down"></i></span>
-                </p>
-            </div>
+
+    milestones.innerHTML = `${milestonesData
+    .map(function (milestone) {
+      return `<div class="milestone border-b" id="${milestone._id}">
+      <div class="flex">
+        <div class="checkbox"><input type="checkbox" onclick="markMileStone(this, ${
+          milestone._id
+        })" /></div>
+        <div onclick="openMilestone(this, ${milestone._id})">
+          <p>
+            ${milestone.name}
+            <span><i class="fas fa-chevron-down"></i></span>
+          </p>
         </div>
-        <div class="hidden_panel">
-            ${milestone.modules.map(function(module){
-                return ` <div class="module border-b">
-                ${module.name}
-            </div>`
-            }).join("")}
-        </div>
-    </div>
-</div>`
-    }).join("")}`
+      </div>
+      <div class="hidden_panel">
+        ${milestone.modules
+          .map(function (module) {
+            return `<div class="module border-b">
+            <p>${module.name}</p>
+          </div>`;
+          })
+          .join("")}
+      </div>
+    </div>`;
+    })
+    .join("")}`;
 }
-function openMilestone(milestoneElement){
-const currentPanel=milestoneElement.parentNode.nextElementSibling;
-currentPanel.classList.toggle('show');
+
+function openMilestone(milestoneElement, id) {
+  const currentPanel = milestoneElement.parentNode.nextElementSibling;
+  const shownPanel = document.querySelector(".show");
+  const active = document.querySelector(".active");
+
+  if (active && !milestoneElement.classList.contains("active")) {
+    active.classList.remove("active");
+  }
+
+  
+  milestoneElement.classList.toggle("active");
+
+  
+  if (!currentPanel.classList.contains("show") && shownPanel)
+    shownPanel.classList.remove("show");
+
+  
+  currentPanel.classList.toggle("show");
+
+  showMilestone(id);
 }
-loadMilestone();
+
+function showMilestone(id) {
+  const milestoneImage = document.querySelector(".milestoneImage");
+  const name = document.querySelector(".title");
+  const details = document.querySelector(".details");
+
+  milestoneImage.style.opacity = "0";
+  milestoneImage.src = milestonesData[id].image;
+  name.innerText = milestonesData[id].name;
+  details.innerText = milestonesData[id].description;
+}
+const milestoneImage = document.querySelector(".milestoneImage");
+milestoneImage.onload = function () {
+  this.style.opacity = "1";
+};
+
+function markMileStone(checkbox, id) {
+  const doneList = document.querySelector(".doneList");
+  const milestonesList = document.querySelector(".milestones");
+  const item = document.getElementById(id);
+
+  if (checkbox.checked) {
+    
+    milestonesList.removeChild(item);
+    doneList.appendChild(item);
+  } else {
+    
+    milestonesList.appendChild(item);
+    doneList.removeChild(item);
+
+    
+  }
+}
+
+loadMilestones();
